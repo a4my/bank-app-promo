e9bd786fc9cb79a141a4872c1919a9ec9962bcc3
Alex Fourmy <alexfourmy@gmail.com>
2021-06-08 15:19:44 +0200

    added smooth scrolling header button

--- script.js
+++ script.js
@@ -29,3 +29,30 @@ document.addEventListener('keydown', function (e) {
         closeModal();
     }
 });
+
+// Add and Remove cookie box
+
+const message = document.createElement('div')
+const header = document.querySelector('.header')
+message.classList.add('cookie-message')
+message.innerHTML =  'We use cookies for improved functionality and analytics. <button class="btn btn--close--cookie">Got it!</button>'
+message.style.backgroundColor = '#37383d'
+// message.style.width = '105%'
+// message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px'
+
+message.style.height = getComputedStyle(message).height + '80px'
+
+header.append(message)
+
+document.querySelector('.btn--close--cookie').addEventListener('click', function() {
+    message.remove()
+})
+
+// Smooth scrolling buttons
+
+const btnScrollTo = document.querySelector('.btn--scroll-to')
+const section1 = document.querySelector('#section--1')
+
+btnScrollTo.addEventListener('click', function (e) {
+    section1.scrollIntoView({ behavior: 'smooth' });
+  });
