c88b6378879dfd270736f0e034739566769600d8
Alex Fourmy <alexfourmy@gmail.com>
2021-06-09 11:23:39 +0200

    completed reveal on scroll effect

--- script.js
+++ script.js
@@ -140,20 +140,6 @@ nav.addEventListener('mouseout', handleHover.bind(1))
 
 //Intersection Observer API
 
-// const obsCallback = function(entries, observer) {
-//     entries.forEach(entry => {
-
-//     })
-// }
-
-// const obsOptions = {
-//     root: null,
-//     threshold: [0, 0.2]
-// }
-
-// const observer = new IntersectionObserver(obsCallback, obsOptions)
-// observer.observe(section1)
-
 const headerSection = document.querySelector('.header')
 const navHeight = nav.getBoundingClientRect().height
 
@@ -171,3 +157,27 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 })
 
 headerObserver.observe(headerSection)
+
+
+///////////////////
+// Reveal sections
+const allSections = document.querySelectorAll('.section')
+
+const revealSection = function(entries, observer) {
+    const [entry] = entries
+
+    if(!entry.isIntersecting) return
+
+    entry.target.classList.remove('section--hidden')
+    observer.unobserve(entry.target)
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15,
+})
+
+allSections.forEach(function(section) {
+    sectionObserver.observe(section)
+    section.classList.add('section--hidden')
+})
