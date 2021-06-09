b473bdd784d69cc1aeb9aa651ac6c5fcc9c460fc
Alex Fourmy <alexfourmy@gmail.com>
2021-06-09 13:51:18 +0200

    completed section 3 slider

--- script.js
+++ script.js
@@ -210,3 +210,45 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+
+////////////////////
+// slider
+
+const slides = document.querySelectorAll('.slide')
+const btnLeft = document.querySelector('.slider__btn--left')
+const btnRight = document.querySelector('.slider__btn--right')
+
+slides.forEach((s, i) =>s.style.transform = `translateX(${100 * i})`)
+
+let curSlide = 0
+const maxSlide = slides.length
+
+const goToSlide = function(slide) {
+    slides.forEach((s, i) =>s.style.transform = `translateX(${100 * (i - curSlide)}%)`)
+}
+
+goToSlide(0)
+
+// Next slide
+const nextSlide = function() {
+    if(curSlide === maxSlide - 1) {
+        curSlide = 0
+    } else {
+        curSlide++
+    }
+    
+    goToSlide(curSlide)
+}
+
+const prevSlide = function(){
+    if(curSlide === 0) {
+        curSlide = maxSlide - 1
+    } else {
+        curSlide--
+    }
+    goToSlide(curSlide)
+}
+
+btnRight.addEventListener('click', nextSlide)
+btnLeft.addEventListener('click', prevSlide)
